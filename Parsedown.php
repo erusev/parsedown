e61a6114b03ba1ef93d6354c7cbed0ce39309fdc
Emanuil Rusev <hello@erusev.com>
2015-01-15 02:37:20 +0200

    resolve #175

--- Parsedown.php
+++ Parsedown.php
@@ -32,8 +32,7 @@ class Parsedown
         $this->DefinitionData = array();
 
         # standardize line breaks
-        $text = str_replace("\r\n", "\n", $text);
-        $text = str_replace("\r", "\n", $text);
+        $text = str_replace(array("\r\n", "\r"), "\n", $text);
 
         # remove surrounding line breaks
         $text = trim($text, "\n");
