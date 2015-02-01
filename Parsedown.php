cb8cc57742546648bb5461e66e35a92f2f89538b
Niklas Keller <me@kelunik.com>
2015-02-02 00:58:24 +0100

    Fixes #279

--- Parsedown.php
+++ Parsedown.php
@@ -1402,7 +1402,7 @@ class Parsedown
 
             if (isset($Element['handler']))
             {
-                $markup .= $this->$Element['handler']($Element['text']);
+                $markup .= $this->{$Element['handler']}($Element['text']);
             }
             else
             {
