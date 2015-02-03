b828fe7c8de13a495aab924bf025b2c51abfd1f8
Jeff Standen <jeff@webgroupmedia.com>
2015-02-03 13:58:47 -0800

    Fixes #283

--- Parsedown.php
+++ Parsedown.php
@@ -1197,7 +1197,7 @@ class Parsedown
             return;
         }
 
-        if (preg_match('/^[(]((?:[^ ()]|[(][^ )]+[)])+)(?:[ ]+("[^"]+"|\'[^\']+\'))?[)]/', $remainder, $matches))
+        if (preg_match('/^[(]((?:[^ ()]|[(][^ )]+[)])+)(?:[ ]+("[^"]*"|\'[^\']*\'))?[)]/', $remainder, $matches))
         {
             $Element['attributes']['href'] = $matches[1];
 
--- test/data/inline_link_title.md
+++ test/data/inline_link_title.md
@@ -2,6 +2,10 @@
 
 [double quotes](http://example.com "Title")
 
+[single quotes blank](http://example.com '')
+
+[double quotes blank](http://example.com "")
+
 [space](http://example.com "2 Words")
 
 [parentheses](http://example.com/url-(parentheses) "Title")
