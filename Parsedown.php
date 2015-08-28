a8822f65f45b7818621fc34a765a63ef778cddaa
Kevin Wang <wkevin27@gmail.com>
2015-08-28 16:09:20 +0800

    add the headeranchor feature, refer link https://pythonhosted.org/Markdown/extensions/toc.html. TOC builder can be sublime text toc plugin or other tools.

--- Parsedown.php
+++ Parsedown.php
@@ -105,6 +105,7 @@ class Parsedown
         '~' => array('FencedCode'),
     );
 
+    protected $headercounts = array();
     # ~
 
     protected $unmarkedBlockTypes = array(
@@ -459,6 +460,11 @@ class Parsedown
 
     protected function blockHeader($Line)
     {
+        if ($this->headercounts['0xffff'] == null)
+        {
+            $this->headercounts['0xffff'] = 0;
+        }
+
         if (isset($Line['text'][1]))
         {
             $level = 1;
@@ -475,11 +481,37 @@ class Parsedown
 
             $text = trim($Line['text'], '# ');
 
+            if (preg_match_all('/[a-zA-Z0-9]+/',strtolower(str_replace(' ','-',rtrim(ltrim($text)))),$arr))
+            {
+                foreach ($arr[0] as $ar)
+                {
+                    $headerid .= $ar;
+                }
+            
+                if (isset($this->headercounts[$headerid]))
+                {
+                    $this->headercounts[$headerid] += 1;
+                    $headerid .= '_'.$this->headercounts[$headerid];
+                }
+                else
+                {
+                    $this->headercounts[$headerid] = 0;
+                }
+            }
+            else
+            {
+                $this->headercounts['0xffff'] += 1;
+                $headerid = '_'.$this->headercounts['0xffff'];
+            }
+
             $Block = array(
                 'element' => array(
                     'name' => 'h' . min(6, $level),
                     'text' => $text,
                     'handler' => 'line',
+                    'attributes' => array(
+                        'id' => $headerid,
+                    ),
                 ),
             );
 
