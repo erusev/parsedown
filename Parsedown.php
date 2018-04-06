387ef63888dd957e01f4362221cfc3552ff87f9c
Aidan Woods <aidantwoods@gmail.com>
2018-04-06 20:55:27 +0100

    Replace array reduce with foreach loop for PHP 5.3 compat

--- Parsedown.php
+++ Parsedown.php
@@ -1648,14 +1648,14 @@ class Parsedown
 
     protected function elementsApplyRecursive($closure, array $Elements)
     {
-        return array_reduce(
-            $Elements,
-            function (array $Elements, array $Element) use ($closure) {
-                $Elements[] = $this->elementApplyRecursive($closure, $Element);
-                return $Elements;
-            },
-            array()
-        );
+        $newElements = array();
+
+        foreach ($Elements as $Element)
+        {
+            $newElements[] = $this->elementApplyRecursive($closure, $Element);
+        }
+
+        return $newElements;
     }
 
     protected function element(array $Element)
