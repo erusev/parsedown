c55dbb0d3fdf22e96ef09041eb91f433be45a478
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:18:43 +0000

    Add safe mode and strict mode configurables

---
+++ src/Configurables/StrictMode.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Erusev\Parsedown\Configurables;
+
+use Erusev\Parsedown\Configurable;
+
+final class StrictMode implements Configurable
+{
+    /** @var bool */
+    private $enabled = false;
+
+    /**
+     * @param bool $enabled
+     */
+    public function __construct($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    public function enabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public static function default(): self
+    {
+        return new self(false);
+    }
+}
