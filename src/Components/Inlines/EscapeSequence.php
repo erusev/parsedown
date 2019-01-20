79a38a1ebb075bf9da555b5a9567b6a27a006268
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:35:03 +0000

    Implement EscapeSequence

---
+++ src/Components/Inlines/EscapeSequence.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Erusev\Parsedown\Components\Inlines;
+
+use Erusev\Parsedown\AST\StateRenderable;
+use Erusev\Parsedown\Components\Inline;
+use Erusev\Parsedown\Html\Renderables\RawHtml;
+use Erusev\Parsedown\Parsedown;
+use Erusev\Parsedown\Parsing\Excerpt;
+use Erusev\Parsedown\State;
+
+final class EscapeSequence implements Inline
+{
+    use WidthTrait, DefaultBeginPosition;
+
+    const SPECIALS = '\\`*_{}[]()>#+-.!|~';
+
+    /** @var string */
+    private $html;
+
+    /**
+     * @param string $html
+     */
+    public function __construct($html)
+    {
+        $this->html = $html;
+        $this->width = 2;
+    }
+
+    /**
+     * @param Excerpt $Excerpt
+     * @param State $State
+     * @return static|null
+     */
+    public static function build(Excerpt $Excerpt, State $State)
+    {
+        if (isset($Excerpt->text()[1]) and \strpbrk($c = $Excerpt->text()[1], self::SPECIALS) !== false) {
+            return new self($c);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return RawHtml
+     */
+    public function stateRenderable(Parsedown $_)
+    {
+        return new RawHtml($this->html);
+    }
+}
