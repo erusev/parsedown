db1d0a4999a1b8370e844d1659cf368a90e9c9e9
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:36:55 +0000

    Implement Strikethrough

---
+++ src/Components/Inlines/Strikethrough.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Erusev\Parsedown\Components\Inlines;
+
+use Erusev\Parsedown\AST\Handler;
+use Erusev\Parsedown\AST\StateRenderable;
+use Erusev\Parsedown\Components\Inline;
+use Erusev\Parsedown\Html\Renderables\Element;
+use Erusev\Parsedown\Html\Renderables\Text;
+use Erusev\Parsedown\Parsedown;
+use Erusev\Parsedown\Parsing\Excerpt;
+use Erusev\Parsedown\State;
+
+final class Strikethrough implements Inline
+{
+    use WidthTrait, DefaultBeginPosition;
+
+    /** @var string */
+    private $text;
+
+    /**
+     * @param string $text
+     * @param int $width
+     */
+    public function __construct($text, $width)
+    {
+        $this->text = $text;
+        $this->width = $width;
+    }
+
+    /**
+     * @param Excerpt $Excerpt
+     * @param State $State
+     * @return static|null
+     */
+    public static function build(Excerpt $Excerpt, State $State)
+    {
+        $text = $Excerpt->text();
+
+        if (\strlen($text) < 2) {
+            return null;
+        }
+
+        if ($text[1] === '~' and \preg_match('/^~~(?=\S)(.+?)(?<=\S)~~/', $text, $matches)) {
+            return new self($matches[1], \strlen($matches[0]));
+        }
+
+        return null;
+    }
+
+    /**
+     * @return Handler<Element>
+     */
+    public function stateRenderable(Parsedown $Parsedown)
+    {
+        return new Handler(
+            /** @return Element */
+            function (State $State) use ($Parsedown) {
+                return new Element(
+                    'del',
+                    [],
+                    $State->applyTo($Parsedown->lineElements($this->text))
+                );
+            }
+        );
+    }
+}
