37a76fbddbc316507fe4afb0d1437e99826d4a60
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:39:49 +0000

    Make Parsedown non-extendable and remove comment header

--- src/Parsedown.php
+++ src/Parsedown.php
@@ -38,20 +38,7 @@ use Erusev\Parsedown\Parsing\Excerpt;
 use Erusev\Parsedown\Parsing\Line;
 use Erusev\Parsedown\Parsing\Lines;
 
-#
-#
-# Parsedown
-# http://parsedown.org
-#
-# (c) Emanuil Rusev
-# http://erusev.com
-#
-# For the full license information, view the LICENSE file that was distributed
-# with this source code.
-#
-#
-
-class Parsedown
+final class Parsedown
 {
     # ~
 
