2d700195dddf43ff3a72af03e9febc10afecd7c8
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:13:20 +0000

    Add handler so closures can implement state renderable via the wrapper

---
+++ src/AST/Handler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Erusev\Parsedown\AST;
+
+use Erusev\Parsedown\Html\Renderable;
+use Erusev\Parsedown\State;
+
+/**
+ * @template T as Renderable
+ */
+final class Handler implements StateRenderable
+{
+    /** @var callable(State):T */
+    private $closure;
+
+    /**
+     * @param callable(State):T $closure
+     */
+    public function __construct(callable $closure)
+    {
+        $this->closure = $closure;
+    }
+
+    /**
+     * @param State $State
+     * @return T&Renderable
+     */
+    public function renderable(State $State)
+    {
+        return ($this->closure)($State);
+    }
+}
