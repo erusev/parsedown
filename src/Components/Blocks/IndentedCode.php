d0526aa2e03cc916d43103278235fac029802dbe
Aidan Woods <aidantwoods@gmail.com>
2019-01-20 02:29:40 +0000

    Implement IndentedCode

---
+++ src/Components/Blocks/IndentedCode.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Erusev\Parsedown\Components\Blocks;
+
+use Erusev\Parsedown\AST\StateRenderable;
+use Erusev\Parsedown\Components\Block;
+use Erusev\Parsedown\Components\ContinuableBlock;
+use Erusev\Parsedown\Html\Renderables\Element;
+use Erusev\Parsedown\Html\Renderables\Text;
+use Erusev\Parsedown\Parsedown;
+use Erusev\Parsedown\Parsing\Context;
+use Erusev\Parsedown\State;
+
+final class IndentedCode implements ContinuableBlock
+{
+    use ContinuableBlockDefaultInterrupt, BlockAcquisition;
+
+    /** @var string */
+    private $code;
+
+    /**
+     * @param string $code
+     */
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @param Context $Context
+     * @param Block|null $Block
+     * @param State|null $State
+     * @return static|null
+     */
+    public static function build(
+        Context $Context,
+        Block $Block = null,
+        State $State = null
+    ) {
+        if (isset($Block) and $Block instanceof Paragraph and ! $Context->previousEmptyLines() > 0) {
+            return null;
+        }
+
+        if ($Context->line()->indent() < 4) {
+            return null;
+        }
+
+        return new self($Context->line()->ltrimBodyUpto(4));
+    }
+
+    /**
+     * @param Context $Context
+     * @return self|null
+     */
+    public function continue(Context $Context)
+    {
+        if ($Context->line()->indent() < 4) {
+            return null;
+        }
+
+        $newCode = $this->code;
+
+        if ($Context->previousEmptyLines() > 0) {
+            $newCode .= \str_repeat("\n", $Context->previousEmptyLines());
+        }
+
+        return new self($newCode . "\n" . $Context->line()->ltrimBodyUpto(4));
+    }
+
+    /**
+     * @return Element
+     */
+    public function stateRenderable(Parsedown $_)
+    {
+        return new Element(
+            'pre',
+            [],
+            [new Element('code', [], [new Text($this->code)])]
+        );
+    }
+}
