dfab289a3ded0657ed983d022d6ab2fb874ed441
Aidan Woods <aidantwoods@gmail.com>
2019-01-25 21:18:33 +0000

    Put reused code in a trait so boolean configurables are easy to make

--- src/Configurables/SafeMode.php
+++ src/Configurables/SafeMode.php
@@ -6,32 +6,5 @@ use Erusev\Parsedown\Configurable;
 
 final class SafeMode implements Configurable
 {
-    /** @var bool */
-    private $enabled = false;
-
-    /**
-     * @param bool $enabled
-     */
-    public function __construct($enabled)
-    {
-        $this->enabled = $enabled;
-    }
-
-    /** @return bool */
-    public function isEnabled()
-    {
-        return $this->enabled;
-    }
-
-    /** @return self */
-    public static function enabled()
-    {
-        return new self(true);
-    }
-
-    /** @return self */
-    public static function initial()
-    {
-        return new self(false);
-    }
+    use BooleanConfigurable;
 }
