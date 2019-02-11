d8d483bd6afe9175fe8e7c6ff81e525750e89890
Aidan Woods <aidantwoods@gmail.com>
2019-02-11 00:12:19 +0000

    Add some component level tests

---
+++ tests/src/StateTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Erusev\Parsedown\Tests;
+
+use Erusev\Parsedown\Configurables\Breaks;
+use Erusev\Parsedown\Configurables\SafeMode;
+use Erusev\Parsedown\Configurables\StrictMode;
+use Erusev\Parsedown\State;
+use PHPUnit\Framework\TestCase;
+
+final class StateTest extends TestCase
+{
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     */
+    public function testStateMerge()
+    {
+        $State = new State;
+
+        $this->assertFalse($State->get(SafeMode::class)->isEnabled());
+        $this->assertFalse($State->get(StrictMode::class)->isEnabled());
+        $this->assertFalse($State->get(Breaks::class)->isEnabled());
+
+        $UpdatedState = $State->mergingWith(new State([SafeMode::enabled()]));
+
+        $this->assertTrue($UpdatedState->get(SafeMode::class)->isEnabled());
+        $this->assertFalse($UpdatedState->get(StrictMode::class)->isEnabled());
+        $this->assertFalse($UpdatedState->get(Breaks::class)->isEnabled());
+    }
+}
