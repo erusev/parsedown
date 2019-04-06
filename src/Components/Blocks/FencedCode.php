3f74fe83479cad2d150730327a9a1a89b8945377
Aidan Woods <aidantwoods@gmail.com>
2019-04-06 18:15:28 +0100

    Port fix for https://github.com/erusev/parsedown/issues/699 into new codebase

--- src/Components/Blocks/FencedCode.php
+++ src/Components/Blocks/FencedCode.php
@@ -118,7 +118,23 @@ final class FencedCode implements ContinuableBlock
      */
     public function stateRenderable()
     {
-        $infostring = $this->infostring();
+        /**
+         * https://www.w3.org/TR/2011/WD-html5-20110525/elements.html#classes
+         * Every HTML element may have a class attribute specified.
+         * The attribute, if specified, must have a value that is a set
+         * of space-separated tokens representing the various classes
+         * that the element belongs to.
+         * [...]
+         * The space characters, for the purposes of this specification,
+         * are U+0020 SPACE, U+0009 CHARACTER TABULATION (tab),
+         * U+000A LINE FEED (LF), U+000C FORM FEED (FF), and
+         * U+000D CARRIAGE RETURN (CR).
+         */
+        $infostring = \substr(
+            $this->infostring(),
+            0,
+            \strcspn($this->infostring(), " \t\n\f\r")
+        );
 
         return new Element('pre', [], [new Element(
             'code',
