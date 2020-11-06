2830754a6894f9fc2f5946c65fb35b59be0d8ba1
Johnny Lee-Othon <jleeothon@icloud.com>
2020-11-06 12:55:55 +0100

    Make autoloading more standard using classmaps

--- test/TrustDelegatedExtension.php
+++ test/TrustDelegatedExtension.php
@@ -1,24 +1,5 @@
 <?php
 
-class UnsafeExtension extends Parsedown
-{
-    protected function blockFencedCodeComplete($Block)
-    {
-        $text = $Block['element']['element']['text'];
-        unset($Block['element']['element']['text']);
-
-        // WARNING: There is almost always a better way of doing things!
-        //
-        // This example is one of them, unsafe behaviour is NOT needed here.
-        // Only use this if you trust the input and have no idea what
-        // the output HTML will look like (e.g. using an external parser).
-        $Block['element']['element']['rawHtml'] = "<p>$text</p>";
-
-        return $Block;
-    }
-}
-
-
 class TrustDelegatedExtension extends Parsedown
 {
     protected function blockFencedCodeComplete($Block)
