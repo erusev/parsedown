67126e85ef48d4c67df0916b3911924e2cc90e3c
SilverServersCopiliot <111811784+SilverServersCopiliot@users.noreply.github.com>
2023-08-04 12:00:44 -0700

    Add image sizing support
    Add image sizing support by allowing the following options:
    #[width]x[height]
    #[width]x
    #x[height]

--- Parsedown.php
+++ Parsedown.php
@@ -1379,9 +1379,10 @@ class Parsedown
         );
 
         //get width and height from href attribute if exists (ex: "http://www.example.com/image.png#100x100")
-        if (preg_match('/^(.*)#(\d+)x(\d+)$/', $Link['element']['attributes']['href'], $matches)) {
+        if (preg_match('/^(.*)#(\d*)x(\d*)$/', $Link['element']['attributes']['href'], $matches)) {
             if(isset($matches[2])) $Inline['element']['attributes']['width']  = $matches[2];
             if(isset($matches[3])) $Inline['element']['attributes']['height'] = $matches[3];
+            $Inline['element']['attributes']['src'] = $matches[1];
         }
 
         $Inline['element']['attributes'] += $Link['element']['attributes'];
